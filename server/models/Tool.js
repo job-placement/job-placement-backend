36be497fb614b630fbf0afd1bc956f93fa37cb5d
cesarsnina <cesar.nina05@gmail.com>
2022-05-03 16:26:07 +0000

    Created the table model for the Job, Language, Tool, and User

---
+++ server/models/Tool.js
@@ -0,0 +1,12 @@
+const { db, DataTypes, Model } = require('../db');
+
+class Tool extends Model {};
+
+Tool.init({
+  name: DataTypes.STRING
+} , {
+	sequelize: db,
+	timestamps: false,
+});
+
+module.exports = { Tool };
