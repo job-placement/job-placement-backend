015f4db933a0885850dcf65fd8b729d0f60bd620
cesarsnina <cesar.nina05@gmail.com>
2022-05-03 20:07:12 +0000

    Created a few json file with data to display

---
+++ server/models/Skill.js
@@ -0,0 +1,12 @@
+const { db, DataTypes, Model } = require('../db');
+
+class Skill extends Model {};
+
+Skill.init({
+  name: DataTypes.STRING
+} , {
+	sequelize: db,
+	timestamps: false,
+});
+
+module.exports = { Skill };
