8e2772d2c95e8dc9ad8010bad8e88d65f4f84ce6
cesarsnina <cesar.nina05@gmail.com>
2022-05-04 16:59:32 +0000

    Added skill as a replacement and edit json for better readability

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 const { Job } = require('./Job');
 const { Language } = require('./Language');
+const { Skill } = require('./Skill');
 const { Tool } = require('./Tool');
 const { User } = require('./User');
 const { db } = require('../db');
@@ -8,17 +9,23 @@ const { db } = require('../db');
 Job.belongsToMany(User, { through: 'Apply' });
 User.belongsToMany(Job, { through: 'Apply' });
 
+// creates a mode called JobTool with JobId, ToolId
+Job.belongsToMany(Skill, { through: 'JobSkill' });
+Skill.belongsToMany(Job, { through: 'JobSkill' });
+
+// Use this these relation if we use Language and Tool model
 // creates a mode called JobLang with JobId, LanguageId
-Job.belongsToMany(Language, { through: 'JobLang' });
-Language.belongsToMany(Job, { through: 'JobLang' });
+// Job.belongsToMany(Language, { through: 'JobLang' });
+// Language.belongsToMany(Job, { through: 'JobLang' });
 
 // creates a mode called JobTool with JobId, ToolId
-Job.belongsToMany(Tool, { through: 'JobTool' });
-Tool.belongsToMany(Job, { through: 'JobTool' });
+// Job.belongsToMany(Tool, { through: 'JobTool' });
+// Tool.belongsToMany(Job, { through: 'JobTool' });
 
 module.exports = {
     Job,
     Language,
+    Skill,
     Tool,
     User,
     db
