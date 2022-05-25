5df499eaa46752407e12ec6993a898409a3b016e
Danny Reina <dannyreina@google.com>
2022-05-25 18:32:28 +0000

    added TODOS

--- api/v1/routes/user.js
+++ api/v1/routes/user.js
@@ -1,5 +1,27 @@
-/*
+const express = require(express)
+const router = express.Router()
+const { body } = require('express-validator')
+const User = require('../models/User')
+const userController = require('../controllers/usersController')
 
-user endpoints
+/*
+TODO: apply DRY principle to redundant endpoint addresses
+
+router.route('/login')
+     .get(userController.getLogin)
+     .post(userController.postLogin)
 
 */
+
+router.get('/login', userController.getLogin)
+
+router.post('/login', userController.postLogin)
+
+router.get('/signup', userController.getSignup)
+
+router.post('/signup', userController.postSignup)
+
+router.post('/logout', userController.postLogout)
+
+
+module.exports = router
