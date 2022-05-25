fe3d9bbcce6af5fd20c1dd73fc63076353cf6de4
sergethi <nsergearistide@gmail.com>
2022-05-25 21:47:03 +0000

    created action for user signup

--- api/v1/controllers/usersController.js
+++ api/v1/controllers/usersController.js
@@ -5,6 +5,7 @@ CRUD actions
 */
 
 const { User } = require('../models/index')
+const bcrypt = require('bcrypt');
 
 const getUsers = async (request, response, next) => {
   try {
@@ -41,6 +42,26 @@ const getSignup = async (request, response, next) => {
 
 const postSignup = async (request, response, next) => {
   try {
+    const newUser = {
+      firstName: request.body.firstName,
+      lastName: request.body.lastName,
+      email: request.body.email,
+      password: request.body.password,
+    };
+    bcrypt.hash(newUser.password, 3, async function(err, encrypted){
+
+      let userCreated = await User.create({
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        email: newUser.email,
+        password: encrypted
+      });
+
+      if (userCreated) {
+        response.send("A new user has been created");
+      }
+    })
+    
 
   } catch (e) {
     console.log(e)
