7b7382d9e04de08ee4ffd4cb00e0c2cbbe587f44
Danny Reina <dannyreina@google.com>
2022-06-02 17:35:19 +0000

    feat: edit jobs action. finish delete job action

--- api/v1/controllers/jobsController.js
+++ api/v1/controllers/jobsController.js
@@ -22,8 +22,10 @@ const createJob = async (request, response, next) => {
   try {
     /* 
     TODO: create global currentUser
+          determine if PK is available in POSTMAN
+          if not then find user by JWT?
     */
-    const user = await User.findByPK(request.userId)
+    const user = await User.findByPK(request.body.userId)
 
     const newJobData = {
       company: request.body.company,
@@ -41,6 +43,41 @@ const createJob = async (request, response, next) => {
 
     await Job.create(newJobData)
 
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const editJob = async (request, response, next) => {
+  try {
+    
+    const user = await User.findByPK(request.body.userId)
+    const job = await Job.findByPK(request.body.jobId) //request.params.jobId ??
+
+    job.set({
+      company: request.body.company,
+      logo: request.body.company,
+      new: request.body.new,
+      featued: request.body.featured,
+      position: request.body.position,
+      role: request.body.role,
+      level: request.body.level,
+      postedAt: request.body.postedAt,
+      contract: request.body.contract,
+      location: request.body.location,
+    })
+
+    await job.save()
+
+  } catch (e) {
+    console.log(e)
+  }
+
+}
+
+const deleteJob = async (request, response, next) => {
+  try {
+    const 
   } catch (e) {
     console.log(e)
   }
