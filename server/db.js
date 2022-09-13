3bdff5085e61192f752bfe8c1d57814bfcf339d8
cesarsnina <cesar.nina05@gmail.com>
2022-09-13 23:33:30 +0000

    testing the db with less information

--- server/db.js
+++ server/db.js
@@ -2,32 +2,38 @@ const { Sequelize, DataTypes, Model } = require('sequelize');
 
 const pkg = require('../package.json');
 
-const pg = require('pg')
+// const pg = require('pg')
 
-let db
-if (process.env.DATABASE_URL) {
-    pg.defaults.ssl = true
-    db = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        logging: false,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        }
-    })
-} else {
-    db = new Sequelize(`postgres://postgres:postgres@localhost:5432/${pkg.name}`, { logging: false})}
+// let db
+// if (process.env.DATABASE_URL) {
+//     pg.defaults.ssl = true
+//     db = new Sequelize(process.env.DATABASE_URL, {
+//         dialect: 'postgres',
+//         protocol: 'postgres',
+//         logging: false,
+//         dialectOptions: {
+//             ssl: {
+//                 require: true,
+//                 rejectUnauthorized: false
+//             }
+//         }
+//     })
+// } else {
+//     db = new Sequelize(`postgres://postgres:postgres@localhost:5432/${pkg.name}`, { logging: false})}
 
+let dialect;
+process.env.NODE_ENV === 'production' ?
+	dialect = { ssl: { require: true, rejectUnauthorized: false }} : {};
 // creates an instance of database called by the apps name
-// const db = new Sequelize(
-//   process.env.DATABASE_URL || `postgres://postgres:postgres@localhost:5432/${pkg.name}`,
-//   {
-//     logging: false
-//   }
-// );
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://postgres:postgres@localhost:5432/${pkg.name}`,
+  {
+		dialect: 'postgres',
+		protocol: 'postgres',
+    logging: false,
+    dialectOptions: dialect
+  }
+);
 
 // Test if the instance is properly connected
 // const testDatabaseConnection = async () => {
