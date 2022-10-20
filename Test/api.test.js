a0d7259cb28d6d8e72f8f5f6279a2e44d5bb0f71
cesarsnina <cesar.nina05@gmail.com>
2022-10-20 04:12:39 +0000

    added a test for /signup and refartor the file

--- Test/api.test.js
+++ Test/api.test.js
@@ -15,67 +15,12 @@ describe('API routes', () => {
 	});
 
 	describe('Users API', () => {
-		let getUser, updateUser, deleteUser, loginUser, newUser;
-
-		describe.skip('Validating Unauthorize User', () => {
-			beforeAll(async () => {
-				getUser = await agent.get('/api/users');
-				updateUser = await agent.put('/api/users').send({
-					firstName: 'Bobby',
-					email: 'bob@gmail.com'
-				});
-				deleteUser = await agent.delete('/api/users');
-				logoutUser = await agent.post('/api/users/logout');
-				const login = await agent
-					.post('/api/users/login')
-					.send({
-						email: 'nmorales@gmail.com',
-						password: 'happy'
-					});
-				loginUser = await agent
-					.post('/api/users/login')
-					.set('cookie', login.headers['set-cookie'])
-					.send({
-						email: 'nmorales@gmail.com',
-						password: 'happy'
-					});
-				newUser = await agent
-					.post('/api/users/signup')
-					.set('cookie', login.headers['set-cookie'])
-					.send({
-						firstName: 'new',
-						lastName: 'user',
-						email: 'newUser@gmail.com',
-						password: 'boom'
-					});
-			});
-			test('should have status 401', () => {
-				expect(getUser.status).toBe(401);
-				expect(updateUser.status).toBe(401);
-				expect(deleteUser.status).toBe(401);
-				expect(logoutUser.status).toBe(401);
-			});
-			test('should have the following message', () => {
-				const message = 'Please log in to proceed';
-				expect(getUser.text).toBe(message);
-				expect(updateUser.text).toBe(message);
-				expect(deleteUser.text).toBe(message);
-				expect(logoutUser.text).toBe(message);
-			});
-			test('should have status 301', async () => {
-				expect(loginUser.status).toBe(301);
-				expect(newUser.status).toBe(301);
-			});
-			test('should have the following message', () => {
-				const message = 'You are already logged in';
-				expect(loginUser.text).toBe(message);
-				expect(newUser.text).toBe(message);
-			});
-		});
-
-		const loggedInMessage = 'You are already logged in';
-		const loginMessage = 'Please log in to proceed';
-		let loggedInUser, loggedOutUser, adminUser, randomUser;
+		const loggedInAlready = 'You are already logged in';
+		const loginToProceed = 'Please log in to proceed';
+		const wrongCredential = 'Incorrect Email or Password!';
+		const missingField = 'All fields must be filled out';
+		const missingCredential = 'Missing credentials';
+		let loggedInUser, adminUser, randomUser, newUser, user;
 		const userCredential = {
 			email: 'nmorales@gmail.com',
 			password: 'happy'
@@ -91,12 +36,12 @@ describe('API routes', () => {
 
 		describe('/api/users/login', () => {
 			test('should log in successfully', async () => {
-				const login = await agent
+				user = await agent
 					.post('/api/users/login')
 					.send(userCredential)
 					.expect(200);
-				loggedInUser = login.headers['set-cookie'];
-				const user = login.body;
+				loggedInUser = user.headers['set-cookie'];
+				user = user.body;
 				expect(user).toHaveProperty('firstName', 'Natalie');
 				expect(user).toHaveProperty('lastName', 'Morales');
 				expect(user).toHaveProperty(
@@ -105,15 +50,14 @@ describe('API routes', () => {
 				);
 			});
 			test('should prevent logged in user to log in', async () => {
-				const user = await agent
+				user = await agent
 					.post('/api/users/login')
 					.set('cookie', loggedInUser)
 					.send(userCredential)
 					.expect(301);
-				expect(user.text).toBe(loggedInMessage);
+				expect(user.text).toBe(loggedInAlready);
 			});
 			test('should return an error for wrong credential', async () => {
-				const message = 'Incorrect Email or Password!';
 				wrongEmail = await agent
 					.post('/api/users/login')
 					.send({
@@ -128,19 +72,28 @@ describe('API routes', () => {
 						password: 'happi'
 					})
 					.expect(401);
-				expect(wrongEmail.body).toBe(message);
-				expect(wrongPassword.body).toBe(message);
+				missingPassword = await agent
+					.post('/api/users/login')
+					.send({
+						email: 'nmorales@gmail.com',
+						password: ''
+					})
+					.expect(401);
+				expect(wrongEmail.body).toBe(wrongCredential);
+				expect(wrongPassword.body).toBe(wrongCredential);
+				expect(missingPassword.body).toBe(
+					missingCredential
+				);
 			});
 		});
 
 		describe('/api/users/logout', () => {
 			test('should successfully logout', async () => {
-				loggedOutUser = await agent
+				user = await agent
 					.post('/api/users/logout')
 					.set('cookie', loggedInUser)
 					.expect(200);
-				const { firstName, lastName, email } =
-					loggedOutUser.body;
+				const { firstName, lastName, email } = user.body;
 				expect(firstName).toBe('Natalie');
 				expect(lastName).toBe('Morales');
 				expect(email).toBe('nmorales@gmail.com');
@@ -149,18 +102,18 @@ describe('API routes', () => {
 				randomUser = await agent
 					.post('/api/users/logout')
 					.expect(401);
-				loggedOutUser = await agent
+				user = await agent
 					.post('/api/users/logout')
 					.set('cookie', loggedInUser)
 					.expect(401);
-				expect(randomUser.text).toBe(loginMessage);
-				expect(loggedOutUser.text).toBe(loginMessage);
+				expect(randomUser.text).toBe(loginToProceed);
+				expect(user.text).toBe(loginToProceed);
 			});
 		});
 
 		describe('/api/users/signup', () => {
 			test('should successfully sign up', async () => {
-				const newUser = await agent
+				newUser = await agent
 					.post('/api/users/signup')
 					.send({
 						firstName: 'new',
@@ -184,10 +137,10 @@ describe('API routes', () => {
 						password: 'boom'
 					})
 					.expect(301);
-				expect(newUser.text).toBe(loggedInMessage);
+				expect(newUser.text).toBe(loggedInAlready);
 			});
 			test('should prevent duplicate email', async () => {
-				const user = await agent
+				user = await agent
 					.post('/api/users/signup')
 					.send({
 						firstName: 'existing',
@@ -198,6 +151,21 @@ describe('API routes', () => {
 					.expect(409);
 				expect(user.text).toBe('Email already exist');
 			});
+			test('should return an error if any field empty', async () => {
+				try {
+					user = await agent
+						.post('/api/users/signup')
+						.send({
+							firstName: 'new',
+							lastName: 'user',
+							password: 'boo'
+						})
+						.expect(400);
+					expect(user.text).toBe(missingField);
+				} catch (error) {
+					expect(error).toMatch('error');
+				}
+			});
 		});
 
 		describe.skip('/api/users', () => {
@@ -207,9 +175,16 @@ describe('API routes', () => {
 		});
 	});
 
-	// describe('Jobs API', () => {});
+	describe('Jobs API', () => {
+		describe.skip('/api/jobs', () => {
+			test('should', () => {});
+		});
+		describe.skip('/api/jobs/:jobId', () => {
+			test('should', () => {});
+		});
+	});
 
-	describe.skip('Skills API', () => {
+	describe('Skills API', () => {
 		describe('/api/skills', () => {
 			let skills;
 			beforeAll(async () => {
@@ -222,6 +197,8 @@ describe('API routes', () => {
 			test('should contain the following skills', () => {
 				expect(skills).toContain('Java');
 				expect(skills).toContain('Golang');
+				expect(skills).toContain('React');
+				expect(skills).toContain('HTML');
 			});
 		});
 	});
